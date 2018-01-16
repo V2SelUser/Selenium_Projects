35dac183a086d4d9c3d409fee260ce6878c18ffc
V2SelUser <vinod@v2softindia.com>
2018-01-16 21:39:56 +0530

    Sample script

--- iConnectX/src/Test_Cases/Dry_Run_Script.java
+++ iConnectX/src/Test_Cases/Dry_Run_Script.java
@@ -1,45 +1,35 @@
 package Test_Cases;
 
-import java.util.Properties;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
-import org.openqa.jetty.jetty.servlet.SessionManager.Session;
-
-public class Dry_Run_Script {
-
-//	public static void main(String[] args) {  
-//		String host="mail.javatpoint.com";  
-//		final String user="sonoojaiswal@javatpoint.com";//change accordingly  
-//		final String password="xxxxx";//change accordingly   
-//		String to="sonoojaiswal1987@gmail.com";//change accordingly  
-//
-//		//Get the session object  
-//		Properties props = new Properties();  
-//		props.put("mail.smtp.host",host);  
-//		props.put("mail.smtp.auth", "true");  
-//
-//		Session session = Session.getDefaultInstance (props);
-//			new javax.mail.Authenticator() {  
-//			protected PasswordAuthentication getPasswordAuthentication() {  
-//				return new PasswordAuthentication(user,password);  
-//			}  
-//	});  
-// 
-//	//Compose the message  
-//		try {  
-//			MimeMessage message = new MimeMessage(session);  
-//			message.setFrom(new InternetAddress(user));  
-//			message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));  
-//			message.setSubject("javatpoint");  
-//			message.setText("This is simple program of sending email using JavaMail API");  
-//  
-//			//send the message  
-//			Transport.send(message);  
-//
-//			System.out.println("message sent successfully...");  
-//
-//		} catch (MessagingException e) {e.printStackTrace();}  
-//	}  
+class Simple{
+	
+	
+	public static  void test()
+	{
+		System.out.println("inside test");
+	}
+	public static  void test1()
+	{
+		System.out.println("inside test");
+	}
+	
+	
+	
 }  
 
-			
+class Dry_Run_Script{  
 
+  
+	  public static void main(String args[]) throws Exception
+	  {  
+		
+int i = 0;
+	for(;;) {
+		System.out.println(i++);
+	}
+
+
+	 }
+}  
